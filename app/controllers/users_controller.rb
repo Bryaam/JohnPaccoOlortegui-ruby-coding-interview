d67868c0aa76488f731eebfd9a8b61a876524361
Bryam Pacco <bryam.pacco@gmail.com>
2022-03-31 14:32:43 -0500

    Fixing users request to fetch by company

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
   def index
     users = User
-              .by_company(params[:company_identifier])
+              .by_company(params[:company_id])
               .by_username(search_params[:username])
-    render json: users.all
+    render json: users
   end
 
   private
