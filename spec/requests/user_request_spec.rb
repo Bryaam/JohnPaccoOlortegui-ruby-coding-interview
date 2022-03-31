2671c6189b64cb181d1e846d9ffe474c0cd1c80b
Bryam Pacco <bryam.pacco@gmail.com>
2022-03-31 14:42:53 -0500

    Adding user filter by username

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Users", type: :request do
 
       it 'returns only the users for the specified company' do
         get company_users_path(company_1)
-        
+
         expect(result.size).to eq(company_1.users.size)
         expect(result.map { |element| element['id'] } ).to eq(company_1.users.ids)
       end
@@ -35,6 +35,19 @@ RSpec.describe "Users", type: :request do
 
       it 'returns all the users' do
 
+      end
+    end
+
+    context 'when fetching users by username' do
+      let!(:user1) { create(:user, username: 'Johndoe') }
+      let!(:user2) { create(:user, username: 'KatAsd') }
+      let!(:user3) { create(:user, username: 'Doe') }
+
+      it 'returns users that match with the given username' do
+        get users_path, params: { username: 'doe' }
+
+        expect(result.size).to eq(2)
+        expect(result.map { |user| user['id']}).to eq([user1, user3].pluck(:id))
       end
     end
   end
